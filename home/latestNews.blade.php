d7d460481047f4076dff997038de88af5fd33870
Somur Ruteeb <s.ruteeb@gmail.com>
2023-11-11 13:43:44 +0100

    Home

--- home/latestNews.blade.php
+++ home/latestNews.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="pbmit-heading-subheading-style-1 text-center">
             <h4 class="pbmit-subtitle">{{ taal_user_pure(391,'ned') }}why choose us?</h4>
-            <h2 class="pbmit-title">{{ taal_user_pure(392,'ned') }}Our All <strong>Latest News</strong></h2>
-            <p class="blog-six-content">{{ taal_user_pure(392,'ned') }}Our team discussed every single detail to make sure Boo is the most versatile and <br>unique theme created so far. Now were up in the big leagues getting’ our turn at bat.</p>
+            <h2 class="pbmit-title">{{ taal_user_pure(392,'ned') }}Our All <strong>{{ taal_user_pure(503,'ned') }}Latest News</strong></h2>
+            <p class="blog-six-content">{{ taal_user_pure(393,'ned') }}Our team discussed every single detail to make sure Boo is the most versatile and <br>{{ taal_user_pure(504,'ned') }}unique theme created so far. Now were up in the big leagues getting’ our turn at bat.</p>
         </div>
         <div class="row">
             <div class="col-md-6 col-lg-4">
@@ -16,19 +16,19 @@
                         </div>
                         <div class="pbminfotech-box-content">
                             <span class="pbmit-meta-line cat-links">
-                                <a href="blog-large-image.html">{{ taal_user_pure(393,'ned') }}International Moving</a>
+                                <a href="blog-large-image.html">{{ taal_user_pure(394,'ned') }}International Moving</a>
                             </span>
                             <div class="pbminfotech-box-title">
-                                <h4><a href="blog-single-view.html">{{ taal_user_pure(394,'ned') }}Our Home Entertainment has Evolved Significantly</a></h4>
+                                <h4><a href="blog-single-view.html">{{ taal_user_pure(395,'ned') }}Our Home Entertainment has Evolved Significantly</a></h4>
                             </div>
                             <div class="pbminfotech-meta-date">
-                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(492,'ned') }}Aug 08, 2018</span>
+                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(496,'ned') }}Aug 08, 2018</span>
                             </div>
                             <div class="pbminfotech-blogbox-footer-commnent">
                                 <span class="pbmit-blogbox-comment-w">
                                     <a href="blog-single-view.html">
                                         <i class="pbmit-base-icon-comment-1"></i>
-                                        <span class="comments">{{ taal_user_pure(493,'ned') }}3 Comments</span>
+                                        <span class="comments">{{ taal_user_pure(497,'ned') }}3 Comments</span>
                                     </a>
                                 </span>
                             </div>
@@ -46,19 +46,19 @@
                         </div>
                         <div class="pbminfotech-box-content">
                             <span class="pbmit-meta-line cat-links">
-                                <a href="blog-large-image.html">{{ taal_user_pure(395,'ned') }}International Moving</a>
+                                <a href="blog-large-image.html">{{ taal_user_pure(398,'ned') }}International Moving</a>
                             </span>
                             <div class="pbminfotech-box-title">
-                                <h4><a href="blog-single-view.html">{{ taal_user_pure(396,'ned') }}These Are The Voyages of The Starship Enterprise</a></h4>
+                                <h4><a href="blog-single-view.html">{{ taal_user_pure(399,'ned') }}These Are The Voyages of The Starship Enterprise</a></h4>
                             </div>
                             <div class="pbminfotech-meta-date">
-                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(493,'ned') }}Feb 18, 2018</span>
+                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(400,'ned') }}Feb 18, 2018</span>
                             </div>
                             <div class="pbminfotech-blogbox-footer-commnent">
                                 <span class="pbmit-blogbox-comment-w">
                                     <a href="blog-single-view.html">
                                         <i class="pbmit-base-icon-comment-1"></i>
-                                        <span class="comments">{{ taal_user_pure(494,'ned') }}3 Comments</span>
+                                        <span class="comments">{{ taal_user_pure(401,'ned') }}3 Comments</span>
                                     </a>
                                 </span>
                             </div>
@@ -76,19 +76,19 @@
                         </div>
                         <div class="pbminfotech-box-content">
                             <span class="pbmit-meta-line cat-links">
-                                <a href="blog-large-image.html">{{ taal_user_pure(495,'ned') }}Transports Takecare</a>
+                                <a href="blog-large-image.html">{{ taal_user_pure(402,'ned') }}Transports Takecare</a>
                             </span>
                             <div class="pbminfotech-box-title">
-                                <h4><a href="blog-single-view.html">{{ taal_user_pure(397,'ned') }}Three Reasons Visibility Matters in Supply Chain</a></h4>
+                                <h4><a href="blog-single-view.html">{{ taal_user_pure(403,'ned') }}Three Reasons Visibility Matters in Supply Chain</a></h4>
                             </div>
                             <div class="pbminfotech-meta-date">
-                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(496,'ned') }}Jan 21, 2018</span>
+                                <i class="pbmit-base-icon-calendar"></i> <span>{{ taal_user_pure(404,'ned') }}Jan 21, 2018</span>
                             </div>
                             <div class="pbminfotech-blogbox-footer-commnent">
                                 <span class="pbmit-blogbox-comment-w">
                                     <a href="blog-single-view.html">
                                         <i class="pbmit-base-icon-comment-1"></i>
-                                        <span class="comments">{{ taal_user_pure(497,'ned') }}3 Comments</span>
+                                        <span class="comments">{{ taal_user_pure(405,'ned') }}3 Comments</span>
                                     </a>
                                 </span>
                             </div>
--- home/progressSection.blade.php
+++ home/progressSection.blade.php
@@ -7,32 +7,32 @@
             <div class="col-md-12 col-lg-6">
                 <div class="progress-six-box">
                     <div class="pbmit-heading-subheading-style-1">
-                        <h4 class="pbmit-subtitle">{{ taal_user_pure(398,'ned') }}why choose us?</h4>
-                        <h2 class="pbmit-title"><strong>{{ taal_user_pure(399,'ned') }}We give you complete control of your shipment.</strong></h2>
+                        <h4 class="pbmit-subtitle">{{ taal_user_pure(406,'ned') }}why choose us?</h4>
+                        <h2 class="pbmit-title"><strong>{{ taal_user_pure(407,'ned') }}We give you complete control of your shipment.</strong></h2>
                     </div>
-                    <p>{{ taal_user_pure(400,'ned') }}Our team discussed every single detail to make sure Boo is the most versatile and unique theme created so far.</p>
+                    <p>{{ taal_user_pure(408,'ned') }}Our team discussed every single detail to make sure Boo is the most versatile and unique theme created so far.</p>
                     <div class="progressbar pbmit-progress-style-2">
-                        <span class="progress-label">{{ taal_user_pure(484,'ned') }}Road Freight</span>
+                        <span class="progress-label">{{ taal_user_pure(409,'ned') }}Road Freight</span>
                         <div class="progress progress-lg progress-percent-bg">
                             <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
                             </div>
-                            <span class="progress-percent">{{ taal_user_pure(485,'ned') }}90%</span>
+                            <span class="progress-percent">{{ taal_user_pure(410,'ned') }}90%</span>
                         </div>
                     </div>
                     <div class="progressbar pbmit-progress-style-2">
-                        <span class="progress-label">{{ taal_user_pure(486,'ned') }}Road Freight</span>
+                        <span class="progress-label">{{ taal_user_pure(411,'ned') }}Road Freight</span>
                         <div class="progress progress-lg progress-percent-bg">
                             <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%">
                             </div>
-                            <span class="progress-percent">{{ taal_user_pure(487,'ned') }}80%</span>
+                            <span class="progress-percent">{{ taal_user_pure(412,'ned') }}80%</span>
                         </div>
                     </div>
                     <div class="progressbar pbmit-progress-style-2">
-                        <span class="progress-label">{{ taal_user_pure(488,'ned') }}Road Freight</span>
+                        <span class="progress-label">{{ taal_user_pure(413,'ned') }}Road Freight</span>
                         <div class="progress progress-lg progress-percent-bg">
                             <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
                             </div>
-                            <span class="progress-percent">{{ taal_user_pure(489,'ned') }}70%</span>
+                            <span class="progress-percent">{{ taal_user_pure(414,'ned') }}70%</span>
                         </div>
                     </div>
                 </div>
