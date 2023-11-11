4740415300a4e4b991dfae45add3f786675800e6
Somur Ruteeb <s.ruteeb@gmail.com>
2023-11-11 15:07:16 +0100

    team details

--- our-team/team-details/progress.blade.php
+++ our-team/team-details/progress.blade.php
@@ -2,46 +2,46 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h4>Skill</h4>
+                <h4>{{ taal_user_pure(479,'ned') }}Skill</h4>
                 <div class="progressbar">
-                    <span class="progress-label">Professional Services</span>
+                    <span class="progress-label">{{ taal_user_pure(480,'ned') }}Professional Services</span>
                     <div class="progress progress-lg progress-percent-bg">
                         <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:80%">
                         </div>
-                        <span class="progress-percent">80%</span>
+                        <span class="progress-percent">{{ taal_user_pure(481,'ned') }}80%</span>
                     </div>
                 </div>
                 <div class="progressbar">
-                    <span class="progress-label">Smarter Driving</span>
+                    <span class="progress-label">{{ taal_user_pure(482,'ned') }}Smarter Driving</span>
                     <div class="progress progress-lg progress-percent-bg">
                         <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:70%">
                         </div>
-                        <span class="progress-percent">70%</span>
+                        <span class="progress-percent">{{ taal_user_pure(483,'ned') }}70%</span>
                     </div>
                 </div>
                 <div class="progressbar">
-                    <span class="progress-label">Smarter Efficiency</span>
+                    <span class="progress-label">{{ taal_user_pure(484,'ned') }}Smarter Efficiency</span>
                     <div class="progress progress-lg progress-percent-bg">
                         <div class="progress-bar aos aos-init aos-animate" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:92%">
                         </div>
-                        <span class="progress-percent">92%</span>
+                        <span class="progress-percent">{{ taal_user_pure(485,'ned') }}92%</span>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="team-details-work-experience">
-                    <h4>Work Experience</h4>
+                    <h4>{{ taal_user_pure(486,'ned') }}Work Experience</h4>
                     <p>
-                        <strong>2015-Present</strong>
-                        <br>Carpo International
+                        <strong>{{ taal_user_pure(487,'ned') }}2015-Present</strong>
+                        <br>{{ taal_user_pure(488,'ned') }}Carpo International
                     </p>
                     <p>
-                        <strong>2008-2015</strong>
-                        <br>Laser Cutters Specialists
+                        <strong>{{ taal_user_pure(489,'ned') }}2008-2015</strong>
+                        <br>{{ taal_user_pure(490,'ned') }}Laser Cutters Specialists
                     </p>
                     <p>
-                        <strong>2000-2008</strong>
-                        <br>WoodCraft International
+                        <strong>{{ taal_user_pure(491,'ned') }}2000-2008</strong>
+                        <br>{{ taal_user_pure(492,'ned') }}WoodCraft International
                     </p>
                 </div>
             </div>
--- our-team/team-details/wrap.blade.php
+++ our-team/team-details/wrap.blade.php
@@ -1,9 +1,9 @@
 <div class="team-details-wrap">
     <div class="container">
         <div class="team-detail-contant">
-            <h4 class="pbmit-title">Biography</h4>
-            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, aliquam quaerat voluptatem.</p>
-            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem.</p>
+            <h4 class="pbmit-title">{{ taal_user_pure(496,'ned') }}Biography</h4>
+            <p>{{ taal_user_pure(497,'ned') }}Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, aliquam quaerat voluptatem.</p>
+            <p>{{ taal_user_pure(498,'ned') }}Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem.</p>
         </div>
     </div>
 </div> 
